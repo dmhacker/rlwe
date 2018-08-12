23cd02b96e4409048bdc8b7d0f7175f38fc02158
David Hacker <dmhacker@protonmail.com>
2018-08-11 17:33:13 -0700

    Finish most of encryption & decryption & add tests

--- src/rlwe.cpp
+++ src/rlwe.cpp
@@ -1,9 +1,22 @@
 #include "rlwe.hpp"
 
 #include <NTL/ZZ_pX.h>
+#include <NTL/GF2X.h>
 #include <cassert>
 
 using namespace rlwe;
+
+ZZX random::UniformSample(long degree, long field_modulus) {
+  if (field_modulus == 2) {
+    // If the finite field is modulo 2, we can use the GF2X class 
+    return conv<ZZX>(random_GF2X(degree));
+  }
+  else {
+    // Otherwise, we set a temporary modulus and use the ZZ_pX class
+    ZZ_pPush(ZZ(field_modulus));
+    return conv<ZZX>(random_ZZ_pX(degree));
+  }
+}
 
 KeyParameters::KeyParameters(long n0, NTL::ZZ q0, NTL::ZZ t0) : n(n0), q(q0), t(t0) {
   // Assert that n is even, assume that it is a power of 2
@@ -24,12 +37,8 @@ KeyParameters::KeyParameters(long n0, NTL::ZZ q0, NTL::ZZ t0) : n(n0), q(q0), t(
 }
 
 PrivateKey KeyParameters::GeneratePrivateKey() const {
-  // TODO: Draw error & secret polynomials from discrete Gaussian distribution
-  ZZX e;
-  ZZX s;
-
-  // Create private key based off of error & secret polynomials 
-  PrivateKey priv(e, s, *this);
+  // Create private key based off of secret polynomial drawn from polynomial ring over GF2 
+  PrivateKey priv(random::UniformSample(n, 2), *this);
   return priv;
 }
 
@@ -38,33 +47,63 @@ PublicKey KeyParameters::GeneratePublicKey(const PrivateKey & priv) const {
   ZZ_pPush push(q); 
 
   // Compute a, where the coefficients are drawn uniformly from the finite field (integers mod q) 
-  ZZ_pX a = NTL::random_ZZ_pX(n);
+  ZZ_pX a = random_ZZ_pX(n);
 
   // Copy private key parameters into polynomial over finite field
-  ZZ_pX s = conv<ZZ_pX>(priv.GetS());
-  ZZ_pX e = conv<ZZ_pX>(priv.GetE());
+  ZZ_pX s = conv<ZZ_pX>(priv.GetSK());
 
-  // Compute b = a * s + e
+  // TODO: Draw error polynomial from discrete Gaussian distribution
+  ZZ_pX e;
+
+  // Compute b = -(a * s + e)
   ZZ_pX b;
   MulMod(b, a, s, phi);
   b += e;
+  b = -b;
 
   // Create public key based off of a & b polynomials
-  PublicKey pub(conv<ZZX>(a), conv<ZZX>(b), *this);
+  PublicKey pub(conv<ZZX>(b), conv<ZZX>(a), *this);
   return pub;
 }
 
 Ciphertext PublicKey::Encrypt(ZZX plaintext) {
   // Set finite field modulus to be q
-  NTL::ZZ_pPush push(params.GetPlainModulus()); 
+  ZZ_pPush push(params.GetCoeffModulus()); 
 
-  Ciphertext ciphertext(plaintext, plaintext);
+  // Upscale plaintext to be in ciphertext ring
+  ZZ_pX m = conv<ZZ_pX>(plaintext) * conv<ZZ_p>(params.GetCoeffModulus() / params.GetPlainModulus());
+
+  // Draw u from GF2 (coefficients are in integers mod 2)
+  ZZ_pX u = conv<ZZ_pX>(random::UniformSample(params.GetPolyModulusDegree(), 2));
+
+  // TODO: Draw error polynomials from discrete Gaussian distribution
+  ZZ_pX e1;
+  ZZ_pX e2;
+
+  // Set up a temporary buffer to hold the results of multiplications
+  ZZ_pX buffer;
+
+  // c1 = p0 * u + e1 + m
+  MulMod(buffer, conv<ZZ_pX>(p0), u, params.GetPolyModulus());
+  ZZ_pX c1 = buffer + e1 + m;
+
+  // c2 = p1 * u + e2
+  MulMod(buffer, conv<ZZ_pX>(p1), u, params.GetPolyModulus());
+  ZZ_pX c2 = buffer + e2;
+
+  Ciphertext ciphertext(conv<ZZX>(c1), conv<ZZX>(c2));
   return ciphertext;
 }
 
-NTL::ZZX PrivateKey::Decrypt(Ciphertext ciphertext) {
+ZZX PrivateKey::Decrypt(Ciphertext ciphertext) {
   // Set finite field modulus to be q
-  NTL::ZZ_pPush push(params.GetPlainModulus()); 
+  ZZ_pPush push(params.GetCoeffModulus()); 
 
-  return ciphertext.GetC1();
+  // m = c1 + c2 * s
+  ZZ_pX m;
+  MulMod(m, conv<ZZ_pX>(ciphertext.GetC1()), conv<ZZ_pX>(s), params.GetPolyModulus());
+  m += conv<ZZ_pX>(ciphertext.GetC0());
+
+  // Downscale m to be in plaintext ring
+  return conv<ZZX>(m) * params.GetPlainModulus() / params.GetCoeffModulus(); 
 }
--- src/rlwe.hpp
+++ src/rlwe.hpp
@@ -4,6 +4,10 @@
 using namespace NTL;
 
 namespace rlwe {
+  namespace random {
+    ZZX UniformSample(long degree, long field_modulus);
+  }
+
   class PublicKey;
   class PrivateKey;
 
@@ -30,29 +34,29 @@ namespace rlwe {
 
   class Ciphertext {
     private:
+      ZZX c0;
       ZZX c1;
-      ZZX c2;
     public:
       /* Constructors */
-      Ciphertext(ZZX c1_0, ZZX c2_0) : c1(c1_0), c2(c2_0) {}
+      Ciphertext(ZZX c0_0, ZZX c1_0) : c0(c0_0), c1(c1_0) {}
 
       /* Getters */
+      ZZX GetC0() const { return c0; }
       ZZX GetC1() const { return c1; }
-      ZZX GetC2() const { return c2; }
   };
 
   class PublicKey {
     private:
-      ZZX a;
-      ZZX b;
+      ZZX p0;
+      ZZX p1;
       const KeyParameters & params;
     public:
       /* Constructors */
-      PublicKey(ZZX a0, ZZX b0, const KeyParameters & p0) : a(a0), b(b0), params(p0) {}
+      PublicKey(ZZX p0_0, ZZX p1_0, const KeyParameters & params0) : p0(p0_0), p1(p1_0), params(params0) {}
 
       /* Getters */
-      ZZX GetA() const { return a; }
-      ZZX GetB() const { return b; } 
+      ZZX GetP0() const { return p0; }
+      ZZX GetP1() const { return p1; } 
 
       /* Public key encryption */
       Ciphertext Encrypt(ZZX plaintext); 
@@ -60,16 +64,14 @@ namespace rlwe {
 
   class PrivateKey {
     private:
-      ZZX e;
       ZZX s;
       const KeyParameters & params;
     public:
       /* Constructors */
-      PrivateKey(ZZX e0, ZZX s0, const KeyParameters & p0) : e(e0), s(s0), params(p0) {}
+      PrivateKey(ZZX s0, const KeyParameters & params0) : s(s0), params(params0) {}
 
       /* Getters */
-      ZZX GetE() const { return e; }
-      ZZX GetS() const { return s; }
+      ZZX GetSK() const { return s; }
 
       /* Private key decryption */
       ZZX Decrypt(Ciphertext ciphertext);
