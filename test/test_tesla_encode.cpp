23bfb3306130753876ebbef85b0f1e1d76a0483a
David Hacker <dmhacker@protonmail.com>
2018-09-21 12:31:12 -0700

    Rename libsodium cmake module

---
+++ test/test_tesla_encode.cpp
@@ -0,0 +1,39 @@
+#include "catch.hpp"
+#include "../src/tesla.h"
+#include "../src/sample.h"
+
+using namespace rlwe;
+using namespace rlwe::tesla;
+
+TEST_CASE("Encoding a hash value into a polynomial") {
+  KeyParameters params;
+
+  // Generate some random values 
+  unsigned char hsh[crypto_hash_sha256_BYTES];
+  NTL::ZZX p1 = UniformSample(params.GetPolyModulusDegree(), params.GetCoeffModulus()); 
+  NTL::ZZX p2 = UniformSample(params.GetPolyModulusDegree(), params.GetCoeffModulus()); 
+  std::string message = "woweee!";
+  
+  // Produce a hash and initial encoding 
+  Hash(hsh, p1, p2, message);
+  ZZX output = Encode(hsh, params);
+
+  // Count the number of non-zero coefficients
+  int w = 0;
+  for (int i = 0; i <= NTL::deg(output); i++) {
+    NTL::ZZ c = NTL::coeff(output, i);
+    if (c == 1 || c == -1) {
+      w += 1;
+    }
+  }
+
+  // Assert that the number of 1's and -1's in the encoding is equal to the encoding weight 
+  REQUIRE(w == params.GetEncodingWeight());
+
+  // Assert that the output has a degree less than the given polynomial modulus
+  REQUIRE(NTL::deg(output) < params.GetPolyModulusDegree());
+
+  // Assert that the output is deterministic and can be replicated
+  ZZX replicated = Encode(hsh, params);
+  REQUIRE(output == replicated);
+}
